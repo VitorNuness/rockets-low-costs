6d76e2739618a65e45a3d5db7ca65ebeb676580e
VitorNuness <vitor1nuness@gmail.com>
2024-09-04 13:39:46 -0300

    creating validations to name and age

--- back-end/src/Controllers/Auth/register.controller.ts
+++ back-end/src/Controllers/Auth/register.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { RegisterUserDTO } from "../../../src/DTOs/Auth/register_user.dto";
 
 @Controller('register')
 export class RegisterController {
     @Post()
-    register(): void {
+    register(@Body() registerUserDTO: RegisterUserDTO): void {
         return;
     }
 }
--- back-end/test/Feature/Auth/register.spec.ts
+++ back-end/test/Feature/Auth/register.spec.ts
@@ -1,5 +1,5 @@
 import * as request from 'supertest';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { RegisterController } from '../../../src/Controllers/Auth/register.controller';
 
@@ -12,6 +12,7 @@ describe('RegisterController', () => {
     }).compile();
 
     app = moduleRef.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe())
     await app.init();
   })
 
@@ -19,7 +20,88 @@ describe('RegisterController', () => {
     it('should access the register route', () => {
       return request(app.getHttpServer())
         .post('/register')
+        .send({name: "Joe", age: 32})
         .expect(201);
+    });
+
+    it('should require a name', () => {
+      return request(app.getHttpServer())
+        .post('/register')
+        .send({name: '', age: 32})
+        .expect(400)
+        .expect((res) => {
+          expect(res.body.message).toContain('O nome deve ser preenchido.')
+        });
+    });
+
+    it('should name must be a string', () => {
+      return request(app.getHttpServer())
+        .post('/register')
+        .send({name: 0, age: 32})
+        .expect(400)
+        .expect((res) => {
+          expect(res.body.message).toContain('O nome deve ser um texto.')
+        });
+    });
+
+    it('should be max of 50 characters to name', () => {
+      return request(app.getHttpServer())
+        .post('/register')
+        .send({name: 'a'.repeat(51), age: 32})
+        .expect(400)
+        .expect((res) => {
+          expect(res.body.message).toContain('O nome deve conter no máximo 50 carácteres.')
+        });
+    });
+
+    it('should be min of 2 characters to name', () => {
+      return request(app.getHttpServer())
+        .post('/register')
+        .send({name: 'a', age: 32})
+        .expect(400)
+        .expect((res) => {
+          expect(res.body.message).toContain('O nome deve conter no mínimo 2 carácteres.')
+        });
+    });
+
+    it('should require a age', () => {
+      return request(app.getHttpServer())
+        .post('/register')
+        .send({name: 'Joe', age: null})
+        .expect(400)
+        .expect((res) => {
+          expect(res.body.message).toContain('A idade deve ser preenchida.')
+        });
+    });
+
+    it('should age must be a integer', () => {
+      return request(app.getHttpServer())
+        .post('/register')
+        .send({name: 'Joe', age: 'four'})
+        .expect(400)
+        .expect((res) => {
+          expect(res.body.message).toContain('A idade deve ser um número inteiro.')
+        });
+    });
+
+    it('should be max of 110 to age', () => {
+      return request(app.getHttpServer())
+        .post('/register')
+        .send({name: 'Joe', age: 111})
+        .expect(400)
+        .expect((res) => {
+          expect(res.body.message).toContain('A idade máxima permitida são 110 anos.')
+        });
+    });
+
+    it('should be min of 1 to age', () => {
+      return request(app.getHttpServer())
+        .post('/register')
+        .send({name: 'Joe', age: 0})
+        .expect(400)
+        .expect((res) => {
+          expect(res.body.message).toContain('A idade mínima permitida é 1 ano.')
+        });
     });
   });
 
