f5004e82b38db6a7c887f770323c2a3afee67e60
VitorNuness <vitor1nuness@gmail.com>
2024-09-04 17:25:14 -0300

    adding validation for name on login

--- back-end/src/Controllers/Auth/login.controller.ts
+++ back-end/src/Controllers/Auth/login.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
+import { LoginDTO } from '../../DTOs/Auth/login.dto';
 
 @Controller('login')
 export class LoginController {
   @Post()
-  login(@Res() res: Response): any {
+  login(@Body() loginDTO: LoginDTO, @Res() res: Response): any {
     return res.status(HttpStatus.OK).send();
   }
 }
--- back-end/test/Feature/Auth/login.spec.ts
+++ back-end/test/Feature/Auth/login.spec.ts
@@ -3,6 +3,26 @@ import { app } from '../../setup';
 
 describe('LoginController', () => {
   it('should access the login route', () => {
-    return request(app.getHttpServer()).post('/login').expect(200);
+    return request(app.getHttpServer()).post('/login').send({name: 'Joe'}).expect(200);
+  });
+
+  it('should require a name', () => {
+    return request(app.getHttpServer())
+      .post('/login')
+      .send({ name: '' })
+      .expect(400)
+      .expect((res) => {
+        expect(res.body.message).toContain('O nome deve ser preenchido.');
+      });
+  });
+
+  it('should name must be a string', () => {
+    return request(app.getHttpServer())
+      .post('/login')
+      .send({ name: 0 })
+      .expect(400)
+      .expect((res) => {
+        expect(res.body.message).toContain('O nome deve ser um texto.');
+      });
   });
 });
