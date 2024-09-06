566c7f0f7777eb9ebf446810213ac1db52d0176f
VitorNuness <vitor1nuness@gmail.com>
2024-09-06 09:10:47 -0300

    refactoring header verify has user and text to display

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Header from "@/components/layouts/header";
 import MissionsList from "@/components/missions_list";
@@ -7,17 +6,10 @@ import { Center, Spinner } from "@chakra-ui/react";
 import RocketService from "@/services/RocketService";
 
 export default function Home() {
-    const router = useRouter();
     const [missions, setMissions] = useState([]);
     const [rockets, setRockets] = useState([]);
 
     useEffect(() => {
-        const user = sessionStorage?.getItem("user");
-        if (!user) {
-            router.push("auth/login");
-            return;
-        }
-
         async function fetchData() {
             setRockets(await RocketService.fetchRockets());
             setMissions(await RocketService.fetchMissions());
@@ -28,7 +20,7 @@ export default function Home() {
 
     return (
         <Container>
-            <Header text="selecione o lançamento" />
+            <Header beforeUser={"Olá "} afterUser=", selecione o lançamento" />
             {missions.length > 0 ? (
                 <MissionsList missions={missions} rockets={rockets} />
             ) : (
