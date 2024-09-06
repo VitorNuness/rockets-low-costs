cd4bc65287c728a4992edf20b0e9a9ed1f0b7465
VitorNuness <vitor1nuness@gmail.com>
2024-09-06 19:28:07 -0300

    adding modules to manage launches and mission on app

--- back-end/src/Modules/app.module.ts
+++ back-end/src/Modules/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from '../Modules/auth.module';
+import { LaunchModule } from './launch.module';
+import { MissionModule } from './mission.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/rockets_low_costs'),
     AuthModule,
+    LaunchModule,
+    MissionModule,
   ],
   controllers: [],
   providers: [],
